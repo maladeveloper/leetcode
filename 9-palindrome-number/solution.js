7f8575a3451d01a9f176a62cfe592d8d2e1496c1
Malavan Srikumar <malavan.srikumar@zendesk.com>
2023-03-25 15:40:39 +1100

    Ran linter on problem 9

--- 9-palindrome-number/solution.js
+++ 9-palindrome-number/solution.js
@@ -2,18 +2,18 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    const xStr = x.toString()
-    const xStrLen = xStr.length
+const isPalindrome = function (x) {
+  const xStr = x.toString();
+  const xStrLen = xStr.length;
 
-    for(let leftIndex = 0; leftIndex < xStrLen; leftIndex++){
-        const rightIndex = xStrLen - leftIndex - 1
-        const leftDigit = xStr[leftIndex]
-        const rightDigit = xStr[rightIndex]
-        
-        if(leftDigit !== rightDigit){
-            return false
-        }
+  for (let leftIndex = 0; leftIndex < xStrLen; leftIndex += 1) {
+    const rightIndex = xStrLen - leftIndex - 1;
+    const leftDigit = xStr[leftIndex];
+    const rightDigit = xStr[rightIndex];
+
+    if (leftDigit !== rightDigit) {
+      return false;
     }
-    return true
+  }
+  return true;
 };
